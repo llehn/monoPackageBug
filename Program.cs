5ee02358d2c0bc374ebbc4aec6ec13b575ef25cb
Lev Lehn <lev.lehn@kardex.com>
2015-06-24 12:42:20 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO.Packaging;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var package = Package.Open("broken.nupkg");
+            var rels = package.GetRelationshipsByType("anonymous");
+        }
+    }
+}
